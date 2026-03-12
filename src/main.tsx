871e0db89cd3f6431d73a297d37c394e37ba9f98
thanongsaklaoketkich <thanong_777@hotmail.com>
2026-03-12 19:47:18 +0100

    Initial react project with open layers

---
+++ src/main.tsx
@@ -0,0 +1,26 @@
+import { createRoot } from "react-dom/client";
+import { useEffect, useRef } from "react";
+import { Map, View } from "ol";
+import { useGeographic } from "ol/proj.js";
+import TileLayer from "ol/layer/Tile.js";
+import { OSM } from "ol/source.js";
+
+// @ts-ignore
+import "ol/ol.css";
+import VectorLayer from "ol/layer/Vector.js";
+
+useGeographic();
+const kommuneLayer = new VectorLayer();
+const grunnskoleLayer = new VectorLayer();
+const map = new Map({
+  view: new View({ center: [10.7, 59.9], zoom: 8 }),
+  layers: [new TileLayer({ source: new OSM() }), kommuneLayer, grunnskoleLayer],
+});
+
+function Application() {
+  const mapRef = useRef<HTMLDivElement | null>(null);
+  useEffect(() => map.setTarget(mapRef.current!), []);
+  return <div ref={mapRef}></div>;
+}
+
+createRoot(document.getElementById("app")!).render(<Application />);
