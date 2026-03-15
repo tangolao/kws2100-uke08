fe6d64ce24b51b102643381d0b2354ce53225e4a
thanongsaklaoketkich <thanong_777@hotmail.com>
2026-03-15 18:02:31 +0100

    Show grunskoler form database

--- server/server.ts
+++ server/server.ts
@@ -1,13 +1,33 @@
 import { Hono } from "hono";
 import { serve } from "@hono/node-server";
+import pg from "pg";
+
+const postgres = new pg.Pool({
+  user: "postgres",
+});
 
 const app = new Hono();
-
 app.get("/", (c) => c.text("Hello world"));
 
-app.get("/api/grunnskoler", (c) => {
+app.get("/api/grunnskoler", async (c) => {
+  const result = await postgres.query(`
+        select 
+            skolenavn, 
+            antallelever, 
+            st_transform( posisjon, 4326)::json 
+            posisjon
+        from grunnskoler_e39212a4d48d4cf284c6f63f254a3d42.grunnskole
+      `);
   return c.json({
     type: "FeatureCollection",
+    features: result.rows.map(({ skolenavn, antallelever, posisjon }) => ({
+      type: "Feature",
+      properties: {
+        skolenavn,
+        antallelever,
+      },
+      geometry: posisjon,
+    })),
   });
 });
 
